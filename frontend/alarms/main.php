85a7f652e44f9c0e8a4a16d05683efa3945d9f8e
mayediaz <34650999+mayediaz@users.noreply.github.com>
2017-12-19 14:57:58 -0500

    Cambio
    Elimina palabra clave

--- frontend/alarms/main.php
+++ frontend/alarms/main.php
@@ -5,6 +5,6 @@ $conn = &ADONewConnection();
 if($conn -> PConnect("mariadb","infomedia","Inf0m3d14alarms","alarmas") === false)
     echo 'no se puedo conectar';
 */
-$conn = mysql_connect("mariadb","infomedia", "Inf0m3d14alarms") or die ("No se pudo conectar");
-mysql_select_db("alarmas",$conn);
+$conn = mysql_connect("host","user", "clave") or die ("No se pudo conectar");
+mysql_select_db("bd",$conn);
 ?>
