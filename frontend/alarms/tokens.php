7930ac9ce66fd2ec3be34888583253d4ee66ac1a
jhflo <jorgeflorez28@hotmail.com>
2017-12-19 14:39:48 -0500

    up

--- frontend/alarms/tokens.php
+++ frontend/alarms/tokens.php
@@ -6,7 +6,7 @@ foreach($M_usuarios->usuarios as $key => $info)
     if($info['refresh'])
     {
         $url = "https://api.ciscospark.com/v1/access_token";
-        $data = array("grant_type"=>"refresh_token","client_id"=>"CLIENT ID","client_secret"=>"CLIENT SECRET","refresh_token"=>$info['refresh']);
+        $data = array("grant_type"=>"refresh_token","client_id"=>"C14b8fdc411b38ea02bd0c19bd760f5517456953f2f8275bbb8afcec97e48cbda","client_secret"=>"9b4e59ad50f74a6bb88d83c42ff61e8e8d340212b6bf9ad184152b552ad14e72","refresh_token"=>$info['refresh']);
         $options = array(
             'http' => array(
                 'header'  => "Content-type: application/json",
@@ -22,12 +22,8 @@ foreach($M_usuarios->usuarios as $key => $info)
         $M_usuarios->act_token();
     }
 }
-<<<<<<< HEAD
 if((isset($_SERVER["HTTP_HOST"]) && $_SERVER["HTTP_HOST"] == '127.0.0.1'))
 {
     echo "Script Ejecutado";
 }
 ?>
-=======
-?>
->>>>>>> 1cfc1eb9475e5257428359c4d02a89b3154cf016
